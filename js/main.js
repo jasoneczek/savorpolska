424bd61b9111a71fd77a2df6f73f01afe54d0a7d
jasoneczek <github@jasonschaufele.com>
2023-12-30 11:27:18 -0600

    Add JavaScript for subscription form validation

--- js/main.js
+++ js/main.js
@@ -13,3 +13,33 @@ document.getElementById('mobileRecipesLink').addEventListener('click', function 
   const mobileRecipesDropdown = document.getElementById('mobileRecipesDropdown');
   mobileRecipesDropdown.classList.toggle('show');
 })
+
+// Functionality for Subscription form validation
+const form = document.getElementById('subscriptionForm');
+const messageContainer = document.getElementById('messageContainer');
+
+form.onsubmit = function (event) {
+
+  event.preventDefault();
+
+  const emailValue = form.email.value.trim();
+
+  if (emailValue === "") {
+    setMessage("Please enter an email address.");
+  } else if (!isValidEmail(emailValue)) {
+    setMessage("Please enter a valid email.");
+    form.email.value = "";
+  } else {
+    setMessage("Subscription successful!");
+    form.email.value = ""
+  }
+};
+
+function setMessage(message) {
+  messageContainer.innerHTML = message;
+}
+
+function isValidEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
+}
